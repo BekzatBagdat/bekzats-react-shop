170ed9f201eb85a9f733f1b13ceb7edcb26a9e1b
Bekzat Bagdat <paintdex12@gmail.com>
2024-10-07 17:56:19 +0200

    created footer, done styling nav

---
+++ src/components/footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <div className="text-center py-2 mt-10">
+      <p>MyReactStore 2024 &copy; Copyright</p>
+    </div>
+  );
+}
+
+export default Footer;
--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,15 +1,17 @@
 import 'typeface-inter';
 import Nav from '../components/nav';
+import Footer from '../components/footer';
 
 function App() {
   return (
-    <div id="main-container">
+    <div id="main-container" className="flex flex-col min-h-screen">
       <header>
         <Nav />
       </header>
-
       <main></main>
-      <footer></footer>
+      <footer className="mt-auto">
+        <Footer />
+      </footer>
     </div>
   );
 }
