7062bfa72d57cd68dc7809336337136da2705b0d
Bekzat Bagdat <paintdex12@gmail.com>
2024-10-10 05:34:42 +0200

    API call on products in the home page

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { createRoot } from 'react-dom/client';
 import { App } from './pages';
 import './styles/index.css';
 import { BrowserRouter } from 'react-router-dom';
+
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <App />
--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -1,5 +1,13 @@
+import Product from '../components/product';
+
 function HomePage() {
-  return <div>Home page content</div>;
+  return (
+    <div className="mr-10 ml-10 mt-5 ">
+      <div className="flex flex-wrap justify-start">
+        <Product />
+      </div>
+    </div>
+  );
 }
 
 export default HomePage;
